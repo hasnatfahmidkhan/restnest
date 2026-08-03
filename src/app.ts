6e1ce23005731e6e7cacea5b603755fc31ee8a29
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2026-08-03 21:52:25 +0600

    update orgin add only one url

--- src/app.ts
+++ src/app.ts
@@ -26,11 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "https://restnest-frontend.onrender.com",
-      "http://localhost:3000",
-      config.app_url,
-    ],
+    origin: config.app_url,
     credentials: true,
   }),
 );
